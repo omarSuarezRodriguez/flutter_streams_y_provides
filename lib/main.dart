e25e0338c619585e56e9c448036175cd45a920c6
omarsuarezrodriguez <omar.suarez.rod@gmail.com>
2023-02-21 13:26:37 -0500

    project added

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import '/presentation/providers/countdown_provider.dart';
+import 'package:flutter/material.dart';
+import '/presentation/pages/home_page.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: ((_) => CountdownProvider() )),
+      ],
+      child: const MaterialApp(
+        title: 'Material App',
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+        // theme: AppTheme().currentTheme,
+      ),
+    );
+  }
+}
---
+++ lib/presentation/pages/home_page.dart
@@ -0,0 +1,73 @@
+import '../providers/countdown_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomePageView();
+  }
+}
+
+class HomePageView extends StatelessWidget {
+  const HomePageView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final countdownProvider = Provider.of<CountdownProvider>(context);
+
+    return Scaffold(
+      /// Título de la página
+      appBar: AppBar(
+        title: const Text('Cuenta regresiva'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                countdownProvider.setCountdownDuration(const Duration(seconds: 60));
+              },
+              icon: const Icon(Icons.restart_alt_outlined))
+        ],
+      ),
+
+      /// Contador
+      body: const _CounterLabel(),
+
+      /// Play - Stop
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          countdownProvider.startStopTimer();
+        },
+        child: Icon(countdownProvider.isRunning
+            ? Icons.pause
+            : Icons.play_arrow_outlined),
+      ),
+    );
+  }
+}
+
+class _CounterLabel extends StatelessWidget {
+  const _CounterLabel({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Icon(Icons.timer_outlined, color: Colors.blue, size: 60),
+          const SizedBox(width: 10),
+          Text(
+            context.select(
+                (CountdownProvider countdown) => countdown.timeLeftString),
+            style: const TextStyle(fontSize: 50),
+          ),
+        ],
+      ),
+    );
+  }
+}
